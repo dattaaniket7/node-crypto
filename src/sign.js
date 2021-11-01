20c1a0f0f2b45a0dde334b3d4f57830bf4fa7a63
Aniket Datta <datta.aniket7@gmail.com>
2021-11-02 02:29:24 +0530

    signing

---
+++ src/sign.js
@@ -0,0 +1,22 @@
+const { createSign, createVerify } = require('crypto');
+const { publicKey, privateKey } = require('./keypair');
+
+const message = 'hello world!';
+
+/// SIGN
+
+const signer = createSign('rsa-sha256');
+
+signer.update(message);
+
+const signature = signer.sign(privateKey, 'hex');
+
+/// VERIFY
+
+const verifier = createVerify('rsa-sha256');
+
+verifier.update(message);
+
+const isVerified = verifier.verify(publicKey, signature, 'hex');
+
+console.log(`Verified: ${isVerified}`);

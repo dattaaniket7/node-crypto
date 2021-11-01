dc2e29038b506902a708c86f1359802adc01d828
Aniket Datta <datta.aniket7@gmail.com>
2021-11-02 01:46:12 +0530

    salt

--- src/hash.js
+++ src/hash.js
@@ -1,1 +1,21 @@
 const { createHash } = require('crypto');
+
+// Create a string hash
+
+function hash(input) {
+	return createHash('sha256').update(input).digest('hex');
+}
+
+// Compare two hashed passwords
+
+let password = 'hello-world!';
+const hash1 = hash(password);
+console.log(hash1);
+
+/// ... some time later
+
+password = 'hello-world!';
+const hash2 = hash(password);
+console.log(hash2);
+
+console.log(match ? '✔️  good password' : '❌  password does not match');

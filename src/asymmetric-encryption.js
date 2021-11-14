f583cda37dd73eb6693c24c1ca925c84a71e2b9f
Aniket Datta <datta.aniket7@gmail.com>
2021-11-15 00:52:12 +0530

    assymetric-encrypt-update-1

--- src/asymmetric-encryption.js
+++ src/asymmetric-encryption.js
@@ -1,7 +1,7 @@
 const { publicEncrypt, privateDecrypt } = require('crypto');
 const { publicKey, privateKey } = require('./keypair');
 
-const message = 'i am bond, james bond';
+const message = 'i am bond, james bond! 007';
 
 const encryptedData = publicEncrypt(publicKey, Buffer.from(message));
 
